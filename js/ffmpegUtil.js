2ca1560f608c3dac5a0e28770c0f1e8ed54cbb02
zeek0x <0h23k1.n40k1@gmail.com>
2020-12-15 00:23:27 +0900

    Handle multi bytes character filename.

--- js/ffmpegUtil.js
+++ js/ffmpegUtil.js
@@ -12,23 +12,24 @@
 
   const emogifTranscode = async ({ss, duration, w, h, x, y, fps, scale}, file) => {
     const { name } = file;
-    ffmpeg.FS('writeFile', name, await fetchFile(file));
-    const filterComplex = `crop=${w}:${h}:${x}:${y},fps=${fps},yadif,scale=${scale}:-1`
-    const output = '__output__.gif'
 
-    const result =
-      await ffmpeg.run(
-        '-ss', ss,
-        '-t', duration,
-        '-i', name,
-        '-filter_complex', filterComplex,
-        output
-      );
-    console.log(result);
+    // ffmpeg.wasm can't handle multi bytes character.
+    const input = '__input__.' + name.split('.').pop();
+    const output = '__output__.gif'
+    const filterComplex = `crop=${w}:${h}:${x}:${y},fps=${fps},yadif,scale=${scale}:-1`
+
+    console.log('Args:', ss, duration, input, filterComplex, output);
+    ffmpeg.FS('writeFile', input, await fetchFile(file));
+    await ffmpeg.run(
+      '-ss', ss,
+      '-t', duration,
+      '-i', input,
+      '-filter_complex', filterComplex,
+      output
+    );
     const data = ffmpeg.FS('readFile', output);
     return new Blob([data.buffer], { type: 'video/mp4' });
   }
 
   win.emogifTranscode = emogifTranscode;
 })(window);
-
