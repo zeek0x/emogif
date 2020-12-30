c28a040c58f00ad829e288489d6bb16351befbb0
zeek0x <0h23k1.n40k1@gmail.com>
2020-12-30 20:31:16 +0900

    Refactor.

--- js/emogifTranscode.js
+++ js/emogifTranscode.js
@@ -1,36 +1,36 @@
 (async ($) => {
-  'use strict';
+  'use strict'
 
   if (!$.FFmpeg) {
-    console.warn('FFmpeg is not loaded!!!');
-    return;
+    console.warn('FFmpeg is not loaded!!!')
+    return
   }
 
-  const { createFFmpeg, fetchFile } = FFmpeg;
+  const { createFFmpeg, fetchFile } = FFmpeg
   // TODO: Set logger & Utilize logger output as system info.
-  const ffmpeg = createFFmpeg({ log: true });
-  await ffmpeg.load();
+  const ffmpeg = createFFmpeg({ log: true })
+  await ffmpeg.load()
 
   const emogifTranscode = async ({ss, duration, w, h, x, y, fps, scale}, file) => {
-    const { name } = file;
+    const { name } = file
 
     // ffmpeg.wasm can't handle multi bytes character.
-    const input = '__input__.' + name.split('.').pop();
+    const input = '__input__.' + name.split('.').pop()
     const output = '__output__.gif'
     const filterComplex = `crop=${w}:${h}:${x}:${y},fps=${fps},yadif,scale=${scale}:-1`
 
-    console.log('Args:', ss, duration, input, filterComplex, output);
-    ffmpeg.FS('writeFile', input, await fetchFile(file));
+    console.log('Args:', ss, duration, input, filterComplex, output)
+    ffmpeg.FS('writeFile', input, await fetchFile(file))
     await ffmpeg.run(
       '-ss', ss,
       '-t', duration,
       '-i', input,
       '-filter_complex', filterComplex,
       output
-    );
-    const data = ffmpeg.FS('readFile', output);
-    return new Blob([data.buffer], { type: 'video/mp4' });
+    )
+    const data = ffmpeg.FS('readFile', output)
+    return new Blob([data.buffer], { type: 'video/mp4' })
   }
 
-  win.emogifTranscode = emogifTranscode;
-})(window);
+  $.emogifTranscode = emogifTranscode
+})(window)
--- js/index.js
+++ js/index.js
@@ -2,51 +2,51 @@
   'use strict'
   $.onload = async () => {
     // global
-    const SlackEmojiKB = 128;
-    const timeUnit = 0.1;
-    const $file = document.getElementById('file');
-    const $table = document.getElementById('table');
-    const $fps = document.getElementById('fps');
-    const $scale = document.getElementById('scale');
-    const $timer = document.getElementById('timer');
-    const $start = document.getElementById('start');
-    const $end = document.getElementById('end');
-    const $container = document.getElementById('container');
-    const $overlay = document.getElementById('overlay');
-    const $corner = document.getElementById('corner');
-    const $video = document.getElementById('video');
-    const $source = document.getElementById('source');
-    const $imgAnchor = document.getElementById('img-anchor');
-    const $img = document.getElementById('img');
-    const $imgSize = document.getElementById('img-size');
+    const SlackEmojiKB = 128
+    const timeUnit = 0.1
+    const $file = document.getElementById('file')
+    const $table = document.getElementById('table')
+    const $fps = document.getElementById('fps')
+    const $scale = document.getElementById('scale')
+    const $timer = document.getElementById('timer')
+    const $start = document.getElementById('start')
+    const $end = document.getElementById('end')
+    const $container = document.getElementById('container')
+    const $overlay = document.getElementById('overlay')
+    const $corner = document.getElementById('corner')
+    const $video = document.getElementById('video')
+    const $source = document.getElementById('source')
+    const $imgAnchor = document.getElementById('img-anchor')
+    const $img = document.getElementById('img')
+    const $imgSize = document.getElementById('img-size')
 
     // ============================================================
     // File Change -> Video Load -> Style Format
     // ============================================================
 
     const handleChange = (event) => {
-      const file = event.target.files[0];
-      $source.src = window.URL.createObjectURL(file);
-      $video.load();
+      const file = event.target.files[0]
+      $source.src = URL.createObjectURL(file)
+      $video.load()
     }
-    $file.addEventListener('change', handleChange);
+    $file.addEventListener('change', handleChange)
 
     const handleLoadeddataEvent = event => {
-      $fps.max = 30; // TODO: set the video fps
-      $fps.min = scale.min = 1;
-      $fps.value = 10;
-      $scale.max = 100; // [%]
-      $scale.min = 0;
-      $scale.value = 31;
-      $timer.max = $start.max = $end.max = parseInt($video.duration) / timeUnit;
-      $timer.min = $start.min = $end.min = 0;
-      $timer.value = $start.value = 0;
-      setElementW($table, $video.clientWidth);
-      setElementX($overlay, 0);
-      setElementY($overlay, 0);
-      setBottomRight($corner, $overlay);
+      $fps.max = 30 // TODO: set the video fps
+      $fps.min = scale.min = 1
+      $fps.value = 10
+      $scale.max = 100 // [%]
+      $scale.min = 0
+      $scale.value = 31
+      $timer.max = $start.max = $end.max = parseInt($video.duration) / timeUnit
+      $timer.min = $start.min = $end.min = 0
+      $timer.value = $start.value = 0
+      setElementW($table, $video.clientWidth)
+      setElementX($overlay, 0)
+      setElementY($overlay, 0)
+      setBottomRight($corner, $overlay)
     }
-    $video.addEventListener('loadeddata', handleLoadeddataEvent, false);
+    $video.addEventListener('loadeddata', handleLoadeddataEvent, false)
 
     // ============================================================
     // Timer
@@ -56,27 +56,27 @@
     Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
       // Function notation to avoid binding 'this' at declaration time.
       get: function () {
-        return this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2;
+        return this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2
       }
-    });
+    })
 
     const handleClickEvent = event => {
-      $video.playing ? $video.pause() : $video.play();
+      $video.playing ? $video.pause() : $video.play()
     }
 
     const handleTimeUpdateEvent = event => {
-      $timer.value = $video.currentTime / timeUnit;
+      $timer.value = $video.currentTime / timeUnit
     }
 
     const handleInputEvent = ({target: {value}}) => {
-      $video.currentTime = parseInt(value) * timeUnit;
+      $video.currentTime = parseInt(value) * timeUnit
     }
 
-    $video.addEventListener('click', handleClickEvent, false);
-    $video.addEventListener('timeupdate', handleTimeUpdateEvent, false);
-    $timer.addEventListener('input', handleInputEvent, false);
-    $start.addEventListener('input', handleInputEvent, false);
-    $end.addEventListener('input', handleInputEvent, false);
+    $video.addEventListener('click', handleClickEvent, false)
+    $video.addEventListener('timeupdate', handleTimeUpdateEvent, false)
+    $timer.addEventListener('input', handleInputEvent, false)
+    $start.addEventListener('input', handleInputEvent, false)
+    $end.addEventListener('input', handleInputEvent, false)
 
     // ============================================================
     // Drag Move
@@ -87,132 +87,132 @@
       Object.defineProperty(object, 'dragging', {
         writable: true,
         value: false
-      });
+      })
     }
-    [$overlay, $corner].forEach(definePropertyDragging);
+    [$overlay, $corner].forEach(definePropertyDragging)
 
     const handleOverlayMouseDonw = ({which, target}) => {
       if (which === 1) {
-        target.dragging = true;
+        target.dragging = true
       }
     }
 
     const handleCornerMouseDown = ({which, target}) => {
       if (which === 1) {
-        target.dragging = true;
+        target.dragging = true
       }
     }
 
     const handleMouseUp = ({which})  => {
       if (which === 1) {
-        $overlay.dragging = $corner.dragging = false;
+        $overlay.dragging = $corner.dragging = false
       }
     }
 
     const handleMoveEvent = ({movementX, movementY}) => {
       if ($corner.dragging) {
-        zoom(movementX);
-        return;
+        zoom(movementX)
+        return
       }
 
       if ($overlay.dragging) {
-        move(movementX, movementY);
+        move(movementX, movementY)
       }
     }
 
-    $overlay.addEventListener('mousedown', handleOverlayMouseDonw, false);
-    $corner.addEventListener('mousedown', handleCornerMouseDown, false);
-    document.addEventListener('mouseup', handleMouseUp, false);
-    document.addEventListener('mousemove', handleMoveEvent, false);
+    $overlay.addEventListener('mousedown', handleOverlayMouseDonw, false)
+    $corner.addEventListener('mousedown', handleCornerMouseDown, false)
+    document.addEventListener('mouseup', handleMouseUp, false)
+    document.addEventListener('mousemove', handleMoveEvent, false)
 
     // TODO: Fix adhoc...
 
     const move = (dx, dy) => {
-      const {x, y} = getRelativePosition($overlay, $container);
-      setElementX($overlay, x + dx);
-      setElementY($overlay, y + dy);
-      setBottomRight($corner, $overlay);
+      const {x, y} = getRelativePosition($overlay, $container)
+      setElementX($overlay, x + dx)
+      setElementY($overlay, y + dy)
+      setBottomRight($corner, $overlay)
     }
 
     // TODO: Fix adhoc...
 
     const zoom = (dx) => {
-      const { w } = getSize(overlay);
-      const sideLength = w + dx;
-      setElementW($overlay, sideLength);
-      setElementH($overlay, sideLength);
-      setBottomRight($corner, $overlay);
+      const { w } = getSize(overlay)
+      const sideLength = w + dx
+      setElementW($overlay, sideLength)
+      setElementH($overlay, sideLength)
+      setBottomRight($corner, $overlay)
     }
 
     const setBottomRight = (child, parent) => {
-      const {w:pw, h:ph} = getSize(parent);
-      const {w:cw, h:ch} = getSize(child);
-      setElementX(child, pw - cw);
-      setElementY(child, ph - ch);
+      const {w:pw, h:ph} = getSize(parent)
+      const {w:cw, h:ch} = getSize(child)
+      setElementX(child, pw - cw)
+      setElementY(child, ph - ch)
     }
 
     const handleGenerateEmogif = async event => {
-      const ss = parseInt($start.value) * timeUnit + '';
-      const duration = (parseInt($end.value) - parseInt($start.value)) * timeUnit + '';
-      const {x, y} = getRelativePosition($overlay, $container);
-      const {w, h} = getSize($overlay);
-      const fps = parseInt($fps.value);
-      const scale = parseInt(getMaxVideoLength($video) * parseInt($scale.value) / 100);
-      const file = $file.files[0];
+      const ss = parseInt($start.value) * timeUnit + ''
+      const duration = (parseInt($end.value) - parseInt($start.value)) * timeUnit + ''
+      const {x, y} = getRelativePosition($overlay, $container)
+      const {w, h} = getSize($overlay)
+      const fps = parseInt($fps.value)
+      const scale = parseInt(getMaxVideoLength($video) * parseInt($scale.value) / 100)
+      const file = $file.files[0]
 
       const blob =
         await emogifTranscode(
           {ss, duration, x, y, w, h, fps, scale}, file
-        );
+        )
 
-      $imgAnchor.href = $img.src = URL.createObjectURL(blob);
-      $imgSize.textContent = blob.size / 1024 + ' [KB]';
+      $imgAnchor.href = $img.src = URL.createObjectURL(blob)
+      $imgSize.textContent = blob.size / 1024 + ' [KB]'
     }
 
     const handleOverlayDbclick = async event => {
       if ($video.readyState === 0) {
-        return ;
+        return
       }
 
-      await handleGenerateEmogif(event);
+      await handleGenerateEmogif(event)
     }
-    $overlay.addEventListener('dblclick', handleOverlayDbclick);
+    $overlay.addEventListener('dblclick', handleOverlayDbclick)
 
     // ============================================================
     // Element Util
     // ============================================================
 
     const getMaxVideoLength = ({videoWidth:w, videoHeight:h}) => {
-      return w > h ? w : h;
+      return w > h ? w : h
     }
 
     const getSize = element => {
-      const {width:w, height:h} = element.getBoundingClientRect();
-      return {w, h};
+      const {width:w, height:h} = element.getBoundingClientRect()
+      return {w, h}
     }
 
     const getRelativePosition = (rect1, rect2) => {
-      const {x:x1, y:y1} = rect1.getBoundingClientRect();
-      const {x:x2, y:y2} = rect2.getBoundingClientRect();
-      const x = x1 - x2;
-      const y = y1 - y2;
-      return {x, y};
+      const {x:x1, y:y1} = rect1.getBoundingClientRect()
+      const {x:x2, y:y2} = rect2.getBoundingClientRect()
+      const x = x1 - x2
+      const y = y1 - y2
+      return {x, y}
     }
 
     const setElementX = (e, x) => {
-      e.style.left = x + 'px';
+      e.style.left = x + 'px'
     }
 
     const setElementY = (e, y) => {
-      e.style.top = y + 'px';
+      e.style.top = y + 'px'
     }
 
     const setElementW = (e, w) => {
-      e.style.width = w + 'px';
+      e.style.width = w + 'px'
     }
 
     const setElementH = (e, h) => {
-      e.style.height = h + 'px';
+      e.style.height = h + 'px'
     }
   }
-})(window);
+})(window)
